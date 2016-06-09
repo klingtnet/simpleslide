8d01a45a076734b5af1ee435a2d32cf682ae2081
Andreas Linz <klingt.net@gmail.com>
2016-06-09 23:32:03 +0200

    Implement basic build command
    **Warning** an output file will be overwritten without further
    confirmation, double check your command arguments!

--- sslide/cmd/build.go
+++ sslide/cmd/build.go
@@ -1,12 +1,74 @@
 package cmd
 
 import (
+	"bufio"
+	"bytes"
+	"io/ioutil"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/russross/blackfriday"
 	"github.com/spf13/cobra"
+
+	"github.com/klingtnet/simpleslide/lib"
 )
 
+const SEP = "<hr />"
+
+var verbose bool
+
 var buildCmd = &cobra.Command{
-	Use:   "build",
+	Use:   "build [input] [output]",
 	Short: "Builds the slideshow",
 	Run: func(cmd *cobra.Command, args []string) {
+		utils.ExpectExactArgs(cmd, 2, args)
+		data, err := ioutil.ReadFile(args[0])
+		utils.ExitIfErr(err)
+		rendered := blackfriday.MarkdownCommon(data)
+		slides := strings.Split(string(rendered), SEP)
+		utils.PrintIfVerbose(verbose, "Found %d slides\n", len(slides))
+		raw, err := renderTemplate(slides)
+		utils.ExitIfErr(err)
+		utils.ExitIfErr(ioutil.WriteFile(args[1], raw, os.FileMode(0644)))
 	},
+}
+
+func init() {
+	buildCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "print what is being done")
+}
+
+type Content struct {
+	Style  string
+	Script string
+	Slides []string
+}
+
+func renderTemplate(slides []string) ([]byte, error) {
+	htmlTmpl, err := utils.ReadFileAsString("assets/template.html")
+	if err != nil {
+		return nil, err
+	}
+	style, err := utils.ReadFileAsString("assets/style.css")
+	if err != nil {
+		return nil, err
+	}
+	script, err := utils.ReadFileAsString("assets/slides.js")
+	if err != nil {
+		return nil, err
+	}
+	content := Content{
+		Style:  style,
+		Script: script,
+		Slides: slides,
+	}
+
+	tmpl, err := template.New("slideshow").Parse(htmlTmpl)
+	if err != nil {
+		return nil, err
+	}
+	var renderBuf bytes.Buffer
+	byteWriter := bufio.NewWriter(&renderBuf)
+	err = tmpl.Execute(byteWriter, content)
+	return renderBuf.Bytes(), err
 }
